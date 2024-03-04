32b6efbbef70158f0b5731e8399cf38dc4eb62f8
User <user@Users-MacBook-Pro.local>
2024-03-03 20:49:34 -0800

    adding testing for challenge 4 and correcting testing for challenge 6

--- python_challenges/src/six_count_char_occur.py
+++ python_challenges/src/six_count_char_occur.py
@@ -1,12 +1,15 @@
 '''Write a program to count the occurrences of a specific character in a string'''
 
 def get_input():
-    input_str = input("Type a string: ")
-    letter = input("Pick a single character to get count: ")
-    if len(letter) > 1:
-        print("Invalid string or too many letters inputed")
-        return None, None
-    return input_str, letter
+    try:
+        input_str = input("Type a string: ")
+        letter = input("Pick a single character to get count: ")
+        if len(letter) > 1:
+            print("Invalid string or too many letters inputed")
+            return None, None
+        return input_str, letter
+    except:
+        print("Invalid")
 
 def count_char(input_str, letter):
     count = 0
@@ -15,7 +18,10 @@ def count_char(input_str, letter):
             count += 1
     return count
 
-input_str, letter = get_input()
-if input_str is not None and letter is not None:
-    result = count_char(input_str, letter)
-    print("The number of times", letter, "appears in -", input_str, "- is:", result)
+# The get_input function will only be called if the module is run as the main script, 
+# not when it is imported in another module or during test collection.
+if __name__ == '__main__':
+    input_str, letter = get_input()
+    if input_str is not None and letter is not None:
+        result = count_char(input_str, letter)
+        print("The number of times", letter, "appears in -", input_str, "- is:", result)
